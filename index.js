69e5a0dc2377ff444278ea57258591be9664291d
leanghok120 <leanghokoeng5@gmail.com>
2024-07-21 06:59:15 +0700

    Add grid

---
+++ index.js
@@ -0,0 +1,9 @@
+const gridContainer = document.getElementById("grid-container")
+let gridSize = 16;
+
+// Creates a grid of gridSize x gridSize
+for (let i = 0; i < gridSize * gridSize; i++) {
+  const gridItem = document.createElement("div")
+  gridItem.classList.add("grid-item")
+  gridContainer.appendChild(gridItem)
+}
