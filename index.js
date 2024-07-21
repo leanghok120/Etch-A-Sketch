41c35e5c26fa57559336610feb56fff9df19cae1
leanghok120 <leanghokoeng5@gmail.com>
2024-07-21 07:09:20 +0700

    Add hover to draw

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ let gridSize = 16;
 
 // Creates a grid of gridSize x gridSize
 for (let i = 0; i < gridSize * gridSize; i++) {
-  const gridItem = document.createElement("div")
+  const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item")
   gridContainer.appendChild(gridItem)
+
+  gridItem.addEventListener("mouseover", function() {
+    gridItem.classList.add("grid-item-active")
+  })
 }
+
