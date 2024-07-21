e1a9c37121b31da17143baedf174e0e9c6e08809
leanghok120 <leanghokoeng5@gmail.com>
2024-07-21 07:26:03 +0700

    Add grid size buttons

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const gridContainer = document.getElementById("grid-container")
 let gridSize = 16;
 
-// Creates a grid of gridSize x gridSize
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("grid-item")
-  gridContainer.appendChild(gridItem)
+function changeGridSize(size) {
+  gridSize = size
 
-  gridItem.addEventListener("mouseover", function() {
-    gridItem.classList.add("grid-item-active")
-  })
+  gridContainer.textContent = ""
+  createGrid()
+}
+
+function createGrid() {
+  // Creates a grid of gridSize x gridSize
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 15px)`
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item")
+    gridContainer.appendChild(gridItem)
+
+    gridItem.addEventListener("mouseover", function() {
+      gridItem.classList.add("grid-item-active")
+    })
+  }
 }
 
