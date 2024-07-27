dfd77f80363e18ed521b0c47842271f6f29a91cd
leanghok120 <leanghokoeng5@gmail.com>
2024-07-27 08:20:06 +0700

    Format code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const gridContainer = document.getElementById("grid-container")
+const gridContainer = document.getElementById("grid-container");
 let gridSize = 16;
 
 function changeGridSize(size) {
-  gridSize = size
+  gridSize = size;
 
-  gridContainer.textContent = ""
-  createGrid()
+  gridContainer.textContent = "";
+  createGrid();
 }
 
 function createGrid() {
   // Creates a grid of gridSize x gridSize
-  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 15px)`
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 15px)`;
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item")
-    gridContainer.appendChild(gridItem)
+    gridItem.classList.add("grid-item");
+    gridContainer.appendChild(gridItem);
 
-    gridItem.addEventListener("mouseover", function() {
-      gridItem.classList.add("grid-item-active")
-    })
+    gridItem.addEventListener("mouseover", function () {
+      gridItem.classList.add("grid-item-active");
+    });
   }
 }
 
-createGrid()
+createGrid();
